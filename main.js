15c53e3a862b16bdec66efa8ee159400d0cd9f30
Brenton <brentontreb@gmail.com>
2023-08-14 16:38:00 -0600

    Got the table collapse to work properly

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// document
-//     .querySelector('#generateBtn')
-//     .addEventListener('click', function(e){
-//         e.preventDefault();
-//         generateCoverLetter();
-// });
-
-// this contains all of the th's we need to edit
 const tableHead = document.querySelector('.tablepress thead tr');
+const tableData = document.querySelectorAll('td');
+
+console.log(Array.from(tableData));
 
 for (th of tableHead.children) {
     // get the first match of the string column-# 
@@ -14,18 +9,20 @@ for (th of tableHead.children) {
     // console.log(col);
     th.addEventListener('click', function(e){
         clickHeader(col);
+
+        if (e.target.innerHTML.search('(collapsed)') != -1) {
+            e.target.innerHTML =  e.target.innerHTML.replace('(collapsed)', '');
+        } else {
+            e.target.innerHTML += ' (collapsed)'
+        };
 });
 }
 
 function clickHeader(col){
-    console.log(col);
+    for (td of tableData) {
+        // console.log(td.classList)
+        if (td.classList.contains(col)) {
+            td.classList.toggle('hide-column');
+        }
+    };
 }
-
-function getTableHeads(){
-    console.log(document.querySelector('th.column-1').value)
-}
-
-console.log(document.querySelector('.tablepress thead tr').childNodes)
-
-// document.querySelectorAll()
-
